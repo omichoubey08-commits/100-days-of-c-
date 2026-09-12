6ebb214fb79e38c84361cb17af769117451b4257
omichoubey08-commits <omichoubey08@gmail.com>
2026-09-12 13:07:17 +0530

    Create ques19.c

---
+++ ques19.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a <= 0 || b <= 0 || c <= 0)
+        printf("Invalid Triangle");
+    else if (a + b <= c || a + c <= b || b + c <= a)
+        printf("Invalid Triangle");
+    else if (a == b && b == c)
+        printf("Equilateral Triangle");
+    else if (a == b || b == c || a == c)
+        printf("Isosceles Triangle");
+    else
+        printf("Scalene Triangle");
+
+    return 0;
+}
