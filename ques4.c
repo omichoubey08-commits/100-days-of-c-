15a2646f89dd5e52c27f106ce6866e1bed06776b
omichoubey08-commits <omichoubey08@gmail.com>
2026-09-12 12:53:52 +0530

    Add program to calculate area and circumference of circle

---
+++ ques4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    float radius, area, circumference;
+    const float PI = 3.14159;
+
+    scanf("%f", &radius);
+
+    area = PI * radius * radius;
+    circumference = 2 * PI * radius;
+
+    printf("Area = %.2f\n", area);
+    printf("Circumference = %.2f", circumference);
+
+    return 0;
+}
